ede64dfa306d0bb70dfd9fd40186140cfad7743f
Jeffrey-Ede <jeffreyede1@outlook.com>
2017-12-31 18:23:24 +0000

    Hyper-renomalisation ellipse

--- Beanland-Atlas/Beanland-Atlas/commensuration_tilts.cpp
+++ Beanland-Atlas/Beanland-Atlas/commensuration_tilts.cpp
@@ -9,16 +9,16 @@ namespace ba
 	**mats: std::vector<cv::Mat> &, Individual images to extract spots from
 	**spot_pos: std::vector<cv::Point> &, Positions of located spots in aligned diffraction pattern
 	**ellipses: std::vector<std::vector<std::vector<cv::Point>>> &, Positions of the minima and maximal extensions of spot ellipses.
-	**The ellipses are decribed in terms of 4 points, clockwise from the top left as it makes it easy to use them to perform 
+	**The ellipses are decribed in terms of 3 points, clockwise from the top left as it makes it easy to use them to perform 
 	**homomorphic warps. The nesting is each image, each spot in the order of their positions in the positions vector, set of
-	**4 points (1 is extra) desctribing the ellipse, in that order
+	**3 points desctribing the ellipse, in that order
 	**acc: cv::Mat &, Average of the aligned diffraction patterns
 	*/
 	void get_spot_ellipses(std::vector<cv::Mat> &mats, std::vector<cv::Point> &spot_pos, cv::Mat &acc, 
 		std::vector<std::vector<std::vector<cv::Point>>> &ellipses)
 	{
 		//Use the Scharr filtrate of the aligned diffraction patterns to estimate the ellipses
-		std::vector<std::vector<cv::Point>> acc_ellipses(spot_pos.size());
+		std::vector<std::vector<cv::Point>> acc_ellipses;
 		ellipse_sizes(acc, spot_pos, acc_ellipses);
 
 		//Use the 
@@ -64,8 +64,8 @@ namespace ba
 	**est_rad: std::vector<cv::Vec3f> &, Two radii to look for the ellipse between
 	**est_frac: const float, Proportion of highest Scharr filtrate values to use when initially estimating the ellipse
 	**ellipses: std::vector<std::vector<std::vector<cv::Point>>> &, Positions of the minima and maximal extensions of spot ellipses.
-	**The ellipses are decribed in terms of 4 points, clockwise from the top left as it makes it easy to use them to perform 
-	**homomorphic warps. The nesting is each spot in the order of their positions in the positions vector, set of 4 points
+	**The ellipses are decribed in terms of 3 points, clockwise from the top left as it makes it easy to use them to perform 
+	**homomorphic warps. The nesting is each spot in the order of their positions in the positions vector, set of 3 points
 	**(1 is extra) desctribing the ellipse, in that order
 	**ellipse_thresh_frac: const float, Proportion of Schaar filtrate in the region use to get an initial estimage of the ellipse
 	**to use
@@ -77,6 +77,8 @@ namespace ba
 		cv::Mat scharr;
 		scharr_amp(img, scharr);
 
+		ellipses = std::vector<std::vector<cv::Point>>(spot_pos.size());
+
 		//For each ellipse on the image 
 		for (int i = 0; i < spot_pos.size(); i++)
 		{
@@ -85,11 +87,31 @@ namespace ba
 			create_annular_mask(mask, 2*est_rad[i][1]+1, est_rad[i][0], est_rad[i][1]);
 			get_mask_values( img, annulus, mask, cv::Point2i( spot_pos[i].y-est_rad[i][1], spot_pos[i].y-est_rad[i][1] ) );
 
-			//Threshold the 50% highest Scharr filtrate values to get an initial estimate for the ellipse
-			cv::Mat thresh;
-			threshold_proportion(annulus, thresh, ellipse_thresh_frac, cv::THRESH_BINARY, 100, true);
+			hyper_renorm_ellipse(annulus, mask, annulus);
+
+			////Threshold the 50% highest Scharr filtrate values to get an initial estimate for the ellipse
+			//cv::Mat thresh;
+			//unsigned int num_points = threshold_proportion(annulus, thresh, ellipse_thresh_frac, cv::THRESH_BINARY, 100, true);
+			//
+			////Get the points making up the ellipse
+			//byte *p;
+			//std::vector<cv::Point> points(num_points);
+			//for (int j = 0, n = 0; j < thresh.rows; j++)
+			//{
+			//	p = thresh.ptr<byte>(j);
+			//	for (int k = 0; k < thresh.cols; k++)
+			//	{
+			//		//If this is one of the thresholded points, add it to the vector
+			//		if (p[k])
+			//		{
+			//			points[n] = cv::Point(k, j);
+			//			n++;
+			//		}
+			//	}
+			//}
+
+			////Use the thresholded points to estimate the ellipse
 			
-			//Continue tomorrow...
 		}
 	}
 
@@ -158,8 +180,10 @@ namespace ba
 	**proportion
 	**hist_bins: const int, Number of bins in histogram used to determine threshold
 	**non-zero: const bool, If true, only use non-zero values when deciding the threshold. Defaults to false.
+	**Returns:
+	**unsigned int, Number of points in the thresholded image
 	*/
-	void threshold_proportion(cv::Mat &img, cv::Mat &thresh, const float thresh_frac, const int thresh_mode,
+	unsigned int threshold_proportion(cv::Mat &img, cv::Mat &thresh, const float thresh_frac, const int thresh_mode,
 		const int hist_bins, const bool non_zero)
 	{
 		//Get the range of values the threshold is being applied to
@@ -225,5 +249,246 @@ namespace ba
 
 		//Threshold the estimated symmetry center values
 		cv::threshold(img, thresh, thresh_val, 1, thresh_mode);
+
+		return num_non_zero;
+	}
+
+	/*Weight the fit an ellipse to a noisy set of data using hyper-renormalisation. The function generates the coefficients
+	**A0 to A5 when the data is fit to the equation A0*x*x + 2*A1*x*y + A2*y*y + 2*f0*(A3*x + A4*y) + f0*f0*A5 = 0, in
+	**that order
+	**Inputs:
+	**mask: cv::Mat &, Data points to be used are non-zeros
+	**weights: cv::Mat &, Weights of the individual data points
+	**f0: const double, Approximate size of the ellipse. This is arbitrary, but choosing a value close
+	**to the correct size reduces numerical errors
+	**thresh: const float, Iterations will be concluded if the cosine of the angle between successive eigenvectors divided
+	**by the amplitude ratio (larger divided by the smaller) is larger than the threshold
+	**max_iter: const int, The maximum number of iterations to perform
+	**Returns:
+	**std::vector<double>, Coefficients of the conic equation
+	*/
+	std::vector<double> hyper_renorm_conic(cv::Mat &mask, cv::Mat weights, const double f0, const float thresh, 
+		const int max_iter)
+	{
+		//Matrix symmetriciser
+		auto sym = [](Eigen::MatrixXd matrix){ return (matrix + matrix.transpose()) / 2; };
+
+		//Amplitude of the dot product between 2 eigenvectors
+		auto dot_amp = [](Eigen::EigenSolver<Eigen::MatrixXd>::EigenvectorsType &e1, 
+			Eigen::EigenSolver<Eigen::MatrixXd>::EigenvectorsType &e2)
+		{ 
+			std::complex<double> dot = e1(0, 0) * e2(0, 0);
+			for (int i = 1; i < e1.rows(); i++)
+			{
+				dot += e1(i, 0) * e2(i, 0);
+			}
+
+			return std::sqrt(dot.real*dot.real + dot.imag*dot.imag);
+		};
+
+		Eigen::VectorXd e(6);
+		e(0) = 1; e(1) = 0; e(2) = 1; e(3) = 0; e(4) = 0; e(5) = 0;
+
+		//Set values of the V0 matrix elements that don't change
+		Eigen::MatrixXd V0 = Eigen::MatrixXd::Constant(6, 6, 0.0);
+		V0(3, 3) = 4*f0*f0;
+		V0(4, 4) = 4*f0*f0;
+
+		Eigen::MatrixXd M = Eigen::MatrixXd::Constant(6, 6, 0.0);
+		Eigen::MatrixXd N = Eigen::MatrixXd::Constant(6, 6, 0.0);
+		
+		Eigen::VectorXd epsilon(6); 
+		epsilon(5) = f0*f0; //This value is the same in every element of the sum
+		
+		//Construct the matrix M
+		unsigned int num_points;
+		byte *p;
+		float *q;
+		double sum_weights = 0.0;
+		std::vector<Eigen::VectorXd> epsilons;
+        #pragma omp parallel for reduction(sum:num_points), reduction(sum:sum_weights), reduction(sum:M)
+		for (int y = 0, i = 0; y < mask.rows; y++)
+		{
+			p = mask.ptr<byte>(y);
+			q = weights.ptr<float>(y);
+			for (int x = 0; x < mask.cols; x++, num_points++, i++)
+			{
+				if (p[x])
+				{
+					//Calculate contribution to matrix M
+					epsilon(0) = x*x;
+					epsilon(1) = 2*y*x;
+					epsilon(2) = y*y;
+					epsilon(3) = 2*f0*x;
+					epsilon(4) = 2*f0*y;
+
+					epsilons[i] = epsilon;
+
+					M += epsilon*epsilon.transpose();
+
+
+					//Sum weights for matrix N calculation
+					sum_weights += q[x];
+				}
+			}
+		}
+
+		//Get the eigenvalues and eigenvectors of M
+		Eigen::EigenSolver<Eigen::MatrixXd> eigen_sol(M);
+
+		//Structure to combine the eigenvalus and eigenvectors so that they can be coupled for sorting
+		struct eigen_val_vect {
+			double val;
+			Eigen::EigenSolver<Eigen::MatrixXd>::EigenvectorsType vect;
+		};
+
+		//Custom comparison of coupled elements based on angle
+		struct by_eigenvalue { 
+			bool operator()(eigen_val_vect const &a, eigen_val_vect const &b) { 
+				return a.val > b.val;
+			}
+		};
+
+		//Populate the coupled structure
+		std::vector<eigen_val_vect> eigen_val_vect_pairs(6);
+		for (int i = 0; i < 6; i++)
+		{
+			eigen_val_vect_pairs[i].val = eigen_sol.eigenvalues()[i].real();
+			eigen_val_vect_pairs[i].vect = eigen_sol.eigenvectors.col(i);
+		}
+
+		//Use the custom comparison to sort the coupled elements in order of decreasing eigenvalue
+		std::sort(eigen_val_vect_pairs.begin(), eigen_val_vect_pairs.end(), by_eigenvalue());
+
+		//Calculate the psuedoinverse of M of truncated rank 5
+		Eigen::MatrixXd M5 = Eigen::MatrixXd::Constant(6, 6, 0.0);
+		for (int i = 0; i < 5; i++)
+		{
+			M5 += ( eigen_val_vect_pairs[i].vect * eigen_val_vect_pairs[i].vect.transpose() ) / 
+				eigen_val_vect_pairs[i].val;
+		}
+
+		//Normalise the weights so that they add up to 1
+		cv::Mat norm_weights = weights / sum_weights;
+
+		//Construct the matrix N
+		std::vector<Eigen::MatrixXd> V(num_points, V0);
+        #pragma omp parallel for reduction(sum:N)
+		for (int y = 0, i = 0; y < mask.rows; y++)
+		{
+			p = mask.ptr<byte>(y);
+			q = norm_weights.ptr<float>(y);
+			for (int x = 0; x < mask.cols; x++, i++)
+			{
+				if (p[x])
+				{
+					//Store the V0 matrix so that it does not have to keep being recalculated
+					V[i](0, 0) = 4*x*x;
+					V[i](0, 1) = 4*x*y;
+					V[i](0, 3) = 4*f0*x;
+					V[i](1, 0) = 4*x*y;
+					V[i](1, 1) = 4*(x*x + y*y);
+					V[i](1, 2) = 4*x*y;
+					V[i](1, 3) = 4*f0*y;
+					V[i](1, 4) = 4*f0*x;
+					V[i](2, 1) = 4*x*y;
+					V[i](2, 2) = 4*y*y;
+					V[i](2, 4) = 4*f0*y;
+					V[i](3, 0) = 4*f0*x;
+					V[i](3, 1) = 4*f0*y;
+					V[i](4, 1) = 4*f0*x;
+					V[i](4, 2) = 4*f0*y;
+
+					//Add contribution to matrix N
+					N += q[x] * ( V[i] + 2*sym(epsilon*e.transpose()) - (1 / (double)num_points) *
+						( epsilon.dot(M5*epsilon) * V[i] + 2.0 * sym( V[i] * M5 * epsilon * epsilon.transpose() ) ) );
+				}
+			}
+		}
+
+		//Solve N * theta = 1/lambda * M * theta to get the eigenvector theta corresponding to the largest eigenvalue 1/lambda
+		Eigen::EigenSolver<Eigen::MatrixXd> solution(M.inverse() * N);
+
+		//Find the eigenvector with the highest eigenvalue
+		double max_eigenvalue = solution.eigenvalues()[0].real;
+		int max_eigenvalue_idx = 0;
+		for (int i = 1; i < 6; i++)
+		{
+			if (solution.eigenvalues()[i].real > max_eigenvalue)
+			{
+				max_eigenvalue = solution.eigenvalues()[i].real;
+				max_eigenvalue_idx = i;
+			}
+		}
+
+		//Largest eigenvector
+		Eigen::EigenSolver<Eigen::MatrixXd>::EigenvectorsType theta0 = solution.eigenvectors.col(max_eigenvalue_idx);
+
+		//Repeat the calculation, iterativery converging from the HyperLS solution
+		for (int k = 0; k < max_iter; k++)
+		{
+			//Prepare M and N matrices for recalculation
+			M = Eigen::MatrixXd::Constant(6, 6, 0.0);
+			N = Eigen::MatrixXd::Constant(6, 6, 0.0);
+
+			//Recalculate the M and N matrices
+            #pragma omp parallel for reduction(sum:N)
+			for (int y = 0, i = 0; y < mask.rows; y++)
+			{
+				p = mask.ptr<byte>(y);
+				q = norm_weights.ptr<float>(y);
+				for (int x = 0; x < mask.cols; x++, i++)
+				{
+					if (p[x])
+					{
+						//Calculate the reweighting factor
+						double weight = 1.0 / ( theta0.dot( V[i]*theta0 ).real() );
+
+						M += weight * epsilons[i] * epsilons[i].transpose();
+
+						//Add contribution to matrix N
+						N += q[x] * ( weight * ( V[i] + 2*sym(epsilons[i]*e.transpose()) ) - (weight*weight / (double)num_points) *
+							( epsilons[i].dot( M5*epsilons[i] ) * V[i] + 2.0 * sym( V[i] * M5 * epsilons[i] * epsilons[i].transpose() ) ) );
+					}
+				}
+			}
+			//Find the eigenvector with the highest eigenvalue
+			max_eigenvalue = solution.eigenvalues()[0].real;
+			max_eigenvalue_idx = 0;
+			for (int i = 1; i < 6; i++)
+			{
+				if (solution.eigenvalues()[i].real > max_eigenvalue)
+				{
+					max_eigenvalue = solution.eigenvalues()[i].real;
+					max_eigenvalue_idx = i;
+				}
+			}
+
+			//Largest eigenvector
+			Eigen::EigenSolver<Eigen::MatrixXd>::EigenvectorsType theta0_prev = theta0;
+			theta0 = solution.eigenvectors.col(max_eigenvalue_idx);
+
+			//Check if the eigenvector is sufficiently similar to the previous
+			double dot = dot_amp(theta0_prev, theta0);
+			double theta0_prev_size = 0.0, theta0_size = 0.0;
+			for (int i = 0; i < 6; i++)
+			{
+				theta0_prev_size += theta0_prev[i].real()*theta0_prev[i].real() + 
+					theta0_prev[i].imag()*theta0_prev[i].imag();
+				theta0_size += theta0[i].real()*theta0[i].real() + theta0[i].imag()*theta0[i].imag();
+			}
+
+			//Cosine of the angle between the eigenvectors
+			double cos_angle = dot / ( std::sqrt(theta0_prev_size) * std::sqrt(theta0_size) );
+
+			//Ratio of smaller eigenvector amplitude to the larger eigenvector amplitude
+			double amp_ratio = theta0_size > theta0_prev_size ? theta0_prev_size / theta0_size : theta0_size / theta0_prev_size;
+
+			//Check if the eigenvectors of successive angles are sufficiently similar
+			if (cos_angle * amp_ratio > thresh)
+			{
+				break;
+			}
+		}
 	}
 }
--- Beanland-Atlas/Beanland-Atlas/commensuration_tilts.h
+++ Beanland-Atlas/Beanland-Atlas/commensuration_tilts.h
@@ -3,13 +3,15 @@
 #include <includes.h>
 #include <defines.h>
 
+#include <Eigen/Eigenvalues>
+
 namespace ba
 {
 	//Number of bins to use when calculating histogram to determine the which threshold to apply to gradient-based symmetry space
     #define THRESH_PROP_HIST_SIZE 100 
 
 	//Proportion of Schaar filtrate in the region use to get an initial estimage of the ellipse to use
-    #define ELLIPSE_THRESH_FRAC 0.3
+    #define ELLIPSE_THRESH_FRAC 0.4
 
 	/*Get ellipses describing each spot from their Scharr filtrates. Ellipses are checked using heuristic arguments:
 	**ellipse shapes vary smoothly with time and ellipse shaps must be compatible with a projection of an array of
@@ -18,9 +20,9 @@ namespace ba
 	**mats: std::vector<cv::Mat> &, Individual images to extract spots from
 	**spot_pos: std::vector<cv::Point> &, Positions of located spots in aligned diffraction pattern
 	**ellipses: std::vector<std::vector<std::vector<cv::Point>>> &, Positions of the minima and maximal extensions of spot ellipses.
-	**The ellipses are decribed in terms of 4 points, clockwise from the top left as it makes it easy to use them to perform 
+	**The ellipses are decribed in terms of 3 points, clockwise from the top left as it makes it easy to use them to perform 
 	**homomorphic warps. The nesting is each image, each spot in the order of their positions in the positions vector, set of
-	**4 points (1 is extra) desctribing the ellipse, in that order
+	**3 points (1 is extra) desctribing the ellipse, in that order
 	**acc: cv::Mat &, Average of the aligned diffraction patterns
 	*/
 	void get_spot_ellipses(std::vector<cv::Mat> &mats, std::vector<cv::Point> &spot_pos, cv::Mat &acc, 
@@ -40,11 +42,9 @@ namespace ba
 	**est_rad: std::vector<cv::Vec3f> &, Two radii to look for the ellipse between
 	**est_frac: const float, Proportion of highest Scharr filtrate values to use when initially estimating the ellipse
 	**ellipses: std::vector<std::vector<std::vector<cv::Point>>> &, Positions of the minima and maximal extensions of spot ellipses.
-	**The ellipses are decribed in terms of 4 points, clockwise from the top left as it makes it easy to use them to perform 
-	**homomorphic warps. The nesting is each spot in the order of their positions in the positions vector, set of 4 points
+	**The ellipses are decribed in terms of 3 points, clockwise from the top left as it makes it easy to use them to perform 
+	**homomorphic warps. The nesting is each spot in the order of their positions in the positions vector, set of 3 points
 	**(1 is extra) desctribing the ellipse, in that order
-	**ellipse_thresh_frac: const float, Proportion of Schaar filtrate in the region use to get an initial estimage of the ellipse
-	**to use
 	*/
 	void ellipse_sizes(cv::Mat &img, std::vector<cv::Point> spot_pos, std::vector<cv::Vec3f> est_rad, const float est_frac,
 		std::vector<std::vector<cv::Point>> &ellipses, const float ellipse_thresh_frac = ELLIPSE_THRESH_FRAC);
@@ -77,7 +77,26 @@ namespace ba
 	**proportion
 	**hist_bins: const int, Number of bins in histogram used to determine threshold
 	**non-zero: const bool, If true, only use non-zero values when deciding the threshold. Defaults to false.
+	**Returns:
+	**unsigned int, Number of points in the thresholded image
 	*/
-	void threshold_proportion(cv::Mat &img, cv::Mat &thresh, const float thresh_frac, const int thresh_mode = cv::THRESH_BINARY,
+	unsigned int threshold_proportion(cv::Mat &img, cv::Mat &thresh, const float thresh_frac, const int thresh_mode = cv::THRESH_BINARY,
 		const int hist_bins = THRESH_PROP_HIST_SIZE, const bool non_zero = false);
+
+	/*Weight the fit an ellipse to a noisy set of data using hyper-renormalisation. The function generates the coefficients
+	**A0 to A5 when the data is fit to the equation A0*x*x + 2*A1*x*y + A2*y*y + 2*f0*(A3*x + A4*y) + f0*f0*A5 = 0, in
+	**that order
+	**Inputs:
+	**mask: cv::Mat &, Data points to be used are non-zeros
+	**weights: cv::Mat &, Weights of the individual data points
+	**f0: const double, Approximate size of the ellipse. This is arbitrary, but choosing a value close
+	**to the correct size reduces numerical errors
+	**thresh: const float, Iterations will be concluded if the cosine of the angle between successive eigenvectors divided
+	**by the amplitude ratio (larger divided by the smaller) is larger than the threshold
+	**max_iter: const int, The maximum number of iterations to perform
+	**Returns:
+	**std::vector<double>, Coefficients of the conic equation
+	*/
+	std::vector<double> hyper_renorm_conic(cv::Mat &mask, cv::Mat weights, const double f0, const float thresh, 
+		const int max_iter);
 }
